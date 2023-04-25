f3b1fe18a749c228f015a2b3f0ea1f9ef19e5180
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 19:48:21 +0300

    create structure for footer foot

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./footer.css";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
@@ -14,7 +15,13 @@ export default function Footer() {
         </span>
       </div>
       <div id="footer-body">footer</div>
-      <div id="footer-foot">footer</div>
+      <div id="footer-foot">
+        <p>&copy; 2023 Keymden Solutions. All rights reserved.</p>
+        <span>
+          <Link to="/privacy">Privacy Policy</Link>
+          <Link to="/terms">Terms & Conditions</Link>
+        </span>
+      </div>
     </footer>
   );
 }
