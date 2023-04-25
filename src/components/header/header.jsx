4bc5b5192a5b1c1b3ab8a624ca493ae83f327968
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 23:31:53 +0300

    style the haeder

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,21 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
   return (
     <header>
-      <div></div>
-      <NavLink>
-        <Link to="/">Home</Link>
-        <Link to="/modules">Courses</Link>
-        <Link to="/teams">For Organizations</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </NavLink>
+      <div id="header">
+        <Link to="/" className="logo">
+          Keymden
+        </Link>
+        <nav>
+          <Link to="/modules">Courses</Link>
+          <Link to="/teams">For Organizations</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        {/* <button id="contact-us">Contact Us</button> */}
+        <button id="contact-us">Apply</button>
+      </div>
     </header>
   );
 }
