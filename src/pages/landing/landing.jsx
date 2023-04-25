e37a7d77066e89f57fa5a331173cf78743436da3
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 13:15:29 +0300

    create modules and lanoidng compoennt

---
+++ src/pages/landing/landing.jsx
@@ -0,0 +1,5 @@
+import "./landing.css";
+
+export default function Landing() {
+  return <main id="landing"></main>;
+}
---
+++ src/pages/modules/modules.jsx
@@ -0,0 +1,5 @@
+import "./modules.css";
+
+export default function Modules() {
+  return <main id="modules">This is the modules page</main>;
+}
