d41427faa9b405c26411899e2dbdc7381591b137
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 14:03:41 +0300

    ad compoents to routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing/landing";
 import NotFound from "./pages/notfound/notfound";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
