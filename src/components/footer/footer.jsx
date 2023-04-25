7df5d5fe07db7b591fefb41a946bc00136953477
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 14:53:15 +0300

    create the footer structure

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,9 @@
 import "./footer.css";
 
 export default function Footer() {
-  return <footer id="footer">This is the footer</footer>;
+  return (
+    <footer id="footer">
+      <div>footer</div>
+    </footer>
+  );
 }
