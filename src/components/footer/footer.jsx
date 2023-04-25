daaab9b1d517895b17f83a1e20afd2534043ff24
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 19:41:07 +0300

    add react icons

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,13 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer id="footer">
-      <div>footer</div>
+      <div id="footer-head">
+        <h3>Keymden Solutions</h3>
+
+        <span></span>
+      </div>
+      <div id="footer-body">footer</div>
+      <div id="footer-foot">footer</div>
     </footer>
   );
 }
