7477ee9449f122dad9109f17e870a54afde44596
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 13:24:43 +0300

    import notfound route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing/landing";
+import NotFound from "./pages/notfound/notfound";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
