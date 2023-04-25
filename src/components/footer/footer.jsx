b6c2db191fd6818e97acc47c85c69c1ce7b7645a
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 19:44:55 +0300

    create footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,17 @@
 import "./footer.css";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer id="footer">
       <div id="footer-head">
-        <h3>Keymden Solutions</h3>
+        <h3>Keymden</h3>
 
-        <span></span>
+        <span>
+          <FaGithub />
+          <FaLinkedin />
+          <FaTwitter />
+        </span>
       </div>
       <div id="footer-body">footer</div>
       <div id="footer-foot">footer</div>
