9ef29dc7984e780aaddc6aa3d1149f81b5a6d947
Enock <enock.mokua@student.moringaschool.com>
2023-04-25 13:21:54 +0300

    create logic for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/landing/landing";
 function App() {
-
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
