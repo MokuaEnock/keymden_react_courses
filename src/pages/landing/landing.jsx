2e61ecae7af2e774ff5582b1da2071639bbef0e6
Enock <enock.mokua@student.moringaschool.com>
2023-04-26 05:17:35 +0300

    create the structure for container 1

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,10 +1,29 @@
+import { Link } from "react-router-dom";
 import "./landing.css";
 
 export default function Landing() {
   return (
     <main id="landing">
-      <section id="landing-1"></section>
-      {/* <section id="landing-2"></section> */}
+      <section id="landing-1">
+        <div id="landing-1-1">
+          <p>Join the next cohort</p>
+          <h1>
+            Build your skills <br />
+            Any time anywhere
+          </h1>
+          <p>
+            We provide 100% remote courses so that they can fit your needs and
+            for you to study from wherever you want.
+          </p>
+          <span id="landing-buttons">
+            <Link to="/find_course">Find course</Link>
+            <Link to="/apply">Join Keymden</Link>
+          </span>
+        </div>
+        <div id="landing-1-2"></div>
+      </section>
+
+      <section id="landing-2"></section>
       <section id="landing-3">
         <h2>
           Browse our <strong>popular</strong> courses.
@@ -15,7 +34,7 @@ export default function Landing() {
         </p>
         <div id="landing-3-cont"></div>
       </section>
-      
+
       <section id="landing-1"></section>
       <section id="landing-1"></section>
       <section id="landing-1"></section>
