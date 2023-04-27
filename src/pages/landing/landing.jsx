7d38ff2dedefa14895b0745cb5428428b74e78a9
Enock <enock.mokua@student.moringaschool.com>
2023-04-28 02:40:14 +0300

    create course quiz as a global component

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,31 +1,9 @@
 import { Link } from "react-router-dom";
 import "./landing.css";
 import CourseCarousel from "../../components/carousels/course";
+import CourseQuiz from "../../components/CourseQuiz/course";
 
 export default function Landing() {
-  function CourseQuiz() {
-    return (
-      <div id="landing-div-4">
-        <section id="landing-4">
-          <span>
-            <h2>
-              Which course is <strong>right</strong> for you?
-            </h2>
-            <p>
-              With several courses to choose from, it can be difficult to
-              determine which one is the best fit for your schedule and
-              preference. Complete the quiz and well let you know which one we
-              would recommend.
-            </p>
-          </span>
-
-          <span>
-            <button id="quiz-button">Take the Quiz.</button>
-          </span>
-        </section>
-      </div>
-    );
-  }
   return (
     <main id="landing">
       <section id="landing-1">
