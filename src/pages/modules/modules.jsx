bba7e980e89e82c2c3eafbce79de63696bc76195
Enock <enock.mokua@student.moringaschool.com>
2023-04-28 03:38:35 +0300

    stylethe haeder

--- src/pages/modules/modules.jsx
+++ src/pages/modules/modules.jsx
@@ -1,20 +1,23 @@
 import "./modules.css";
 import CourseQuiz from "../../components/CourseQuiz/course";
+import { Link } from "react-router-dom";
 
 export default function Modules() {
   return (
     <main id="modules">
       <section id="modules-1">
         <h2>
-          Discover the <strong>right</strong> course for you.
+          Discover courses in <strong>web</strong> & <strong>data</strong>
         </h2>
         <p>
           Browse through our catalogue find something that excites you and apply
           for the next cohort to start learning today.
         </p>
+        <span>
+          <button>All Courses</button>
+          <button>Take Quiz</button>
+        </span>
       </section>
-
-      <CourseQuiz />
     </main>
   );
 }
