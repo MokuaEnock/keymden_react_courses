f0de09c1c2b49ce432724ecd3162929dd7ae4189
Enock <enock.mokua@student.moringaschool.com>
2023-04-30 06:19:22 +0300

    restyle landing page

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import "./landing.css";
-import CourseCarousel from "../../components/carousels/course";
 import CourseQuiz from "../../components/CourseQuiz/course";
 
 export default function Landing() {
