4d58ecf5eba7859fefcd2ea24dec130407e12b0f
Enock <enock.mokua@student.moringaschool.com>
2023-05-11 17:54:13 +0300

    style the structure of a carosel item and handle the time interval

--- src/components/carousels/course.jsx
+++ src/components/carousels/course.jsx
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from "react";
-import "./car.css";
+import React, { useState, useEffect, useRef } from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import "./car.css";
 
 export default function Carousel() {
-  const [currentItem, setCurrentItem] = useState(0); // State to track the current item index
-  // const carouselArray = [Carousel1, Carousel2, Carousel3, Carousel4, Carousel5];
-  const carouselArray = [Carousel1];
-  let interval; // Variable to hold the interval
+  const [currentItem, setCurrentItem] = useState(0);
+  const carouselArray = [Carousel1, Carousel2, Carousel3, Carousel4, Carousel5];
+  const intervalRef = useRef(null);
 
   useEffect(() => {
     startTimer();
 
     return () => {
-      clearInterval(interval); // Clear the interval when the component is unmounted
+      clearInterval(intervalRef.current);
     };
-  }, []); // Empty dependency array to run the effect only once on mount
+  }, []);
+
+  useEffect(() => {
+    clearInterval(intervalRef.current);
+    startTimer();
+  }, [currentItem]);
 
   function startTimer() {
-    interval = setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setCurrentItem((prevItem) => (prevItem + 1) % carouselArray.length);
-    }, 15000); // Delay of 15 seconds (15000 milliseconds)
+    }, 15000);
   }
 
   function Carousel1() {
@@ -29,13 +33,20 @@ export default function Carousel() {
         <span className="car-section-1" id="car-section-1"></span>
         <span className="car-section-2">
           <h4>Data science</h4>
-          <p></p>
-          <span>
-            <button></button>
-            <button></button>
-            <button></button>
-          </span>
-          <span>
+          <p>
+            Data science involves extracting insights from data using
+            statistical analysis and machine learning techniques. It combines
+            programming, mathematics, and domain knowledge to solve complex
+            problems and make data-driven decisions
+          </p>
+
+          {/* <span className="car-item-attrs">
+            <p>Beginner Friendly</p>
+            <p>8 Months</p>
+            <p>8 Projects</p>
+          </span> */}
+
+          <span className="car-item-links">
             <Link to="">Learn More</Link>
             <Link to="">Enroll</Link>
           </span>
@@ -44,42 +55,34 @@ export default function Carousel() {
     );
   }
 
-  // function Carousel2() {
-  //   return <div className="car-item">item 2</div>;
-  // }
+  function Carousel2() {
+    return <div className="car-item">item 2</div>;
+  }
 
-  // function Carousel3() {
-  //   return <div className="car-item">item 3</div>;
-  // }
+  function Carousel3() {
+    return <div className="car-item">item 3</div>;
+  }
 
-  // function Carousel4() {
-  //   return <div className="car-item">item 4</div>;
-  // }
+  function Carousel4() {
+    return <div className="car-item">item 4</div>;
+  }
 
-  // function Carousel5() {
-  //   return <div className="car-item">item 5</div>;
-  // }
+  function Carousel5() {
+    return <div className="car-item">item 5</div>;
+  }
 
   function handleNext() {
     setCurrentItem((prevItem) => (prevItem + 1) % carouselArray.length);
-    resetTimer();
   }
 
   function handleBack() {
     setCurrentItem(
       (prevItem) => (prevItem - 1 + carouselArray.length) % carouselArray.length
     );
-    resetTimer();
   }
 
   function handleNav(index) {
     setCurrentItem(index);
-    resetTimer();
-  }
-
-  function resetTimer() {
-    clearInterval(interval);
-    startTimer();
   }
 
   return (
