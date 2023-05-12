c665f21e243330bc86a7518e96b0b6017d7c53f2
Enock <enock.mokua@student.moringaschool.com>
2023-05-12 06:48:40 +0300

    create the testimonial card

--- src/components/carousels/testimonials.jsx
+++ src/components/carousels/testimonials.jsx
@@ -2,11 +2,12 @@ import "./tezt.css";
 
 import React, { useState, useEffect, useRef } from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaQuoteRight } from "react-icons/fa";
 
 export default function TesttimonialCarousel() {
   const [currentItem, setCurrentItem] = useState(0);
-  const carouselArray = [Testimonial1, Testimonial2, Testimonial3];
+  //   const carouselArray = [Testimonial1, Testimonial2, Testimonial3];
+  const carouselArray = [Testimonial1];
   const intervalRef = useRef(null);
 
   useEffect(() => {
@@ -31,24 +32,42 @@ export default function TesttimonialCarousel() {
   function Testimonial1() {
     return (
       <div className="testimonial-item" id="testimonial-item1">
-        Testimony 1
+        <span
+          className="testimonial-item-image"
+          id="testimonial-item-image-1"
+        ></span>
+        <span className="testimonial-item-quote">
+          <FaQuoteRight />
+        </span>
+        <span className="testimonial-item-info">
+          <p>
+            Keymden helped upsacle in my career after i took their Data Science
+            course, i secured employemnt 2 months after finishing the course.
+          </p>
+          <h3>Shaman Adebayo</h3>
+          <h5>
+            CTO <strong>Adept Technologies</strong>
+          </h5>
+        </span>
       </div>
     );
   }
-  function Testimonial2() {
-    return (
-      <div className="testimonial-item" id="testimonial-item2">
-        Testimony 2
-      </div>
-    );
-  }
-  function Testimonial3() {
-    return (
-      <div className="testimonial-item" id="testimonial-item3">
-        Testimony 3
-      </div>
-    );
-  }
+
+  //   function Testimonial2() {
+  //     return (
+  //       <div className="testimonial-item" id="testimonial-item2">
+  //         Testimony 2
+  //       </div>
+  //     );
+  //   }
+
+  //   function Testimonial3() {
+  //     return (
+  //       <div className="testimonial-item" id="testimonial-item3">
+  //         Testimony 3
+  //       </div>
+  //     );
+  //   }
 
   function handleNext() {
     setCurrentItem((prevItem) => (prevItem + 1) % carouselArray.length);
